1e975609c39be9d66b70b2e0727cb075f967f9a0
Ben Cochrane <bencochrane1@me.com>
2015-02-09 00:02:44 +1100

    remove table extra for pg issue

--- db/migrate/20150208005509_change_status_id_from_string_to_integer.rb
+++ db/migrate/20150208005509_change_status_id_from_string_to_integer.rb
@@ -1,6 +1,6 @@
 class ChangeStatusIdFromStringToInteger < ActiveRecord::Migration
   def change
-    change_column :leads, :status_id, 'integer USING CAST("column_to_change" AS integer)'
+    change_column :leads, :status_id, :integer
   end
 end
 
