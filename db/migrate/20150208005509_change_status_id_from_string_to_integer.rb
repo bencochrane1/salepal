dc2324bb8a33d4f8a23147cd0b509dbcdfd27d0d
Ben Cochrane <bencochrane1@me.com>
2015-02-08 23:57:07 +1100

    add in migration change column line USING CAST for postgres error when deploying to heroku

--- db/migrate/20150208005509_change_status_id_from_string_to_integer.rb
+++ db/migrate/20150208005509_change_status_id_from_string_to_integer.rb
@@ -1,5 +1,6 @@
 class ChangeStatusIdFromStringToInteger < ActiveRecord::Migration
   def change
-    change_column :leads, :status_id, :integer
+    change_column :leads, :status_id, 'integer USING CAST("column_to_change" AS integer)'
   end
 end
+
