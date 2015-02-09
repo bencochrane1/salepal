4256fe4ace6cd71c8024bc6bd88fe3210de507c0
Ben Cochrane <bencochrane1@me.com>
2015-02-09 17:07:45 +1100

    change redirect to home route to see user name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.from_omniauth(env['omniauth.auth'])
         session[:user_id] = user.id
-        redirect_to '/dashboard', notice: "Signed In Successfully"
+        redirect_to '/', notice: "Signed In Successfully"
     end
 
     def destroy
