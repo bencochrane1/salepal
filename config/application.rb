81593473e3c49e9da12ec0f0c12ad07c1574917b
Ben Cochrane <bencochrane1@me.com>
2015-02-13 08:57:37 +1100

    remove config for rails files present

--- config/application.rb
+++ config/application.rb
@@ -40,27 +40,3 @@ module Salepal
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
-
-# module ViewContext
-#   attr_accessor :output_buffer
-
-#   def output_buffer_with_sprockets=(buffer)
-#     unless is_sprockets?
-#       output_buffer_without_sprockets=(buffer)
-#     end
-#   end
-
-#   def is_sprockets?
-#     self.try(:environment).class == Sprockets::Index
-#   end
-
-#   def self.included(klass)
-#     klass.instance_eval do
-#       alias_method_chain :output_buffer=, :sprockets
-#     end
-#   end
-# end
-
-# Rails.application.assets.context_class.class_eval do
-#    include ViewContext
-# end
