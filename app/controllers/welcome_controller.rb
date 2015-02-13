4ed33a8994f09b0db5a0428329ca3a32b0f1e9d4
Ben Cochrane <bencochrane1@me.com>
2015-02-13 12:38:36 +1100

    add in gmail

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,24 +4,24 @@ class WelcomeController < ApplicationController
 
 
 
-    # connect_to_gmail do |gmail|
-    #   Lead.all.each do |lead|
+    connect_to_gmail do |gmail|
+      Lead.all.each do |lead|
 
-    #     gmail.inbox.find(from: lead.title).each do |email|  
+        gmail.inbox.find(from: lead.title).each do |email|  
 
-    #       begin
-    #         real_to_email = email.to[0].mailbox + "@" + email.to[0].host 
-    #         real_from_email = email.from[0].mailbox + "@" + email.from[0].host 
-    #         current_user.emails.find_or_create_by(to: real_to_email, from: real_from_email, subject: email.subject, message_body: email.text_part.body.raw_source, date_sent: email.to_imap_date, lead_id: lead.id)
+          begin
+            real_to_email = email.to[0].mailbox + "@" + email.to[0].host 
+            real_from_email = email.from[0].mailbox + "@" + email.from[0].host 
+            current_user.emails.find_or_create_by(to: real_to_email, from: real_from_email, subject: email.subject, message_body: email.text_part.body.raw_source, date_sent: email.to_imap_date, lead_id: lead.id)
 
-    #       rescue NoMethodError
+          rescue NoMethodError
 
-    #       end
+          end
 
-    #     end
-    #   end
-    #   gmail.logout
-    # end
+        end
+      end
+      gmail.logout
+    end
 
 
 
